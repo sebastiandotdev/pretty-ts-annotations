4c54f9121e04bf05dda8a6d4c05af1c0949a3bc1
sebastiandotdev <sebastiandotdev@gmail.com>
2025-03-09 22:25:05 -0500

    Update package.json for build scripts

--- src/extension.ts
+++ src/extension.ts
@@ -1,3 +1,12 @@
 import type { ExtensionContext } from 'vscode'
+import { commands, window } from 'vscode'
 
-export function activate(_context: ExtensionContext) {}
+export function activate(context: ExtensionContext) {
+  const disposable = commands.registerCommand('extension.helloWorld', () => {
+    window.showInformationMessage('Hello world!')
+  })
+
+  context.subscriptions.push(disposable)
+}
+
+export function deactivate() {}
